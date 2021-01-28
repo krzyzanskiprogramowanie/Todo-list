91edb2b033b8643cf2ae9f6388ad27573552bfc5
krzyzanskiprogramowanie <73827583+krzyzanskiprogramowanie@users.noreply.github.com>
2021-01-28 19:19:03 +0100

    Solved problem with checkbox drag and drop

--- Todo List/Todo List/TodoList.cs
+++ Todo List/Todo List/TodoList.cs
@@ -19,9 +19,9 @@ namespace Todo_List
             InitializeComponent();
 
             //test value
-            string[] items = new string[] { "item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10", "item11", "item12", "item13", "item14", "item15", "item16", "item17", "item18", "item19", "item20", "item21", "item22", "item23" };
-            string[] items2 = new string[] { "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20", "m21", "m22", "m23" };
-            string[] items3 = new string[] { "em1", "em2", "em3", "em4", "em5", "em6", "em7", "em8", "em9", "em10", "em11", "em12", "em13", "em14", "em15", "em16", "em17", "em18", "em19", "em20", "em21", "em22", "em23" };
+            string[] items = new string[] { "item1", "item2", "item3", "item4", "item5", "item6"};
+            string[] items2 = new string[] { "m1", "m2", "m3", "m4", "m5"};
+            string[] items3 = new string[] { "em1", "em2", "em3", "em4"};
             checkedListBox_ToDo.Items.AddRange(items);
             checkedListBox_doing.Items.AddRange(items2);
             checkedListBox_done.Items.AddRange(items3);
@@ -65,17 +65,6 @@ namespace Todo_List
             }
 
 
-            try
-            {
-                checkedListBox_done.Items.RemoveAt(indexDone);
-
-            }
-            catch (Exception)
-            {
-                checkedListBox_doing.Items.RemoveAt(indexDoing);
-
-            }
-
         }
 
 
@@ -118,17 +107,7 @@ namespace Todo_List
 
             }
 
-            try
-            {
-                checkedListBox_done.Items.RemoveAt(indexDone);
-
-            }
-            catch (Exception)
-            {
-                checkedListBox_ToDo.Items.RemoveAt(indexToDo);
-
-
-            }
+          
 
 
         }
@@ -166,16 +145,6 @@ namespace Todo_List
             check3.Items.Add(e.Data.GetData(typeof(System.String)).ToString());
             try
             {
-                checkedListBox_doing.Items.RemoveAt(indexDoing);
-            }
-            catch (Exception) 
-            {
-                checkedListBox_ToDo.Items.RemoveAt(indexToDo);
-
-            }
-
-            try
-            {
                 checkedListBox_ToDo.Items.RemoveAt(indexToDo);
 
             }
@@ -184,7 +153,6 @@ namespace Todo_List
                 checkedListBox_doing.Items.RemoveAt(indexDoing);
 
             }
-
 
 
         }
